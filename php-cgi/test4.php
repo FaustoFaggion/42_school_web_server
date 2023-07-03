6ee02fa9903171b52c673993758c2bb59e54ca18
FaustoFaggion <faustofaggion@hotmail.com>
2023-07-03 17:48:50 -0300

    Upload File working with php-cgi

--- php-cgi/test4.php
+++ php-cgi/test4.php
@@ -2,15 +2,29 @@
 
 var_dump($_FILES);
 
-echo " run test4.php\n";
+// Check if a file was uploaded
+if (isset($_FILES['userfile']))
+{
+    // Specify the target directory to save the file
+    $targetDirectory = '../locations/uploads';
 
-// Read input from STDIN
-$input = file_get_contents('php://stdin');
-
-// Process the input
-// ...
-
-// Output the response
-echo "Processed input: " . $input;
+    // Get the file name and temporary file path
+    $fileName = $_FILES['userfile']['name'];
+	echo $filename . "\n";
+    // $tempFilePath = "./_TMP_FILE";
+	$tempFilePath = $_FILES['userfile']['tmp_name'];
+	echo $tempFilePath . "\n";
+    // Generate the target file path
+    $targetFilePath = $targetDirectory . "/" . $fileName;
+	echo $targetFilePath . "\n";
+    // Move the uploaded file to the target directory
+    if (move_uploaded_file($tempFilePath, $targetFilePath)) {
+        echo '\n\nFile saved successfully.';
+    } else {
+        echo '\n\nError saving the file.';
+    }
+} else {
+    echo '\n\nNo file uploaded.';
+}
 
 ?>
