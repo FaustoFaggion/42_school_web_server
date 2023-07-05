4799cfed58cfa4ed5dee196be5a9599afc4846ea
FaustoFaggion <faustofaggion@hotmail.com>
2023-07-05 12:19:40 -0300

    Change names into the html and php file to be easer to debug

---
+++ php-cgi/GET_NO_QUERY_STRING.php
@@ -0,0 +1,34 @@
+<?php
+http_response_code(200);
+header("HTTP/1.1 200 OK");
+// header("Status: 200 OK");
+
+echo 'sapi_type: ' . $sapi_type . '\n\n';
+
+        echo "<body>\n";
+
+        echo "<br>";
+        echo "<hr>";
+        echo $_SERVER['SERVER_PROTOCOL'];
+
+        echo "<br>";
+        echo "<hr>";
+        echo "_SERVER[]" . "<br>";
+        foreach ($_SERVER as $key => $value) {
+            echo $key . " = " . $value . "<br>";
+        
+        }
+
+        echo "<br>";
+        echo "_POST[]" . "<br>";
+        foreach ($_POST as $key => $value) {
+            echo $key . " = " . $value . "<br>";
+        }
+
+        echo "<br>";
+        echo "_GET[]" . "<br>";
+        foreach ($_GET as $key => $value) {
+            echo $key . " = " . $value . "<br>";
+        }
+        echo "</body>\n";
+?>
